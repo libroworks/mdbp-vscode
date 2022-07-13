66d2f43d9756ac4c4b44a340f57150124649214f
lwohtsu <ohtsu-y1@libroworks.co.jp>
2022-07-13 14:30:58 +0900

    ひとまず動く状態になった

--- extension.js
+++ extension.js
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const MarkdownBookPreviewServer = require('./lib/markdown-book-preview-server');
+const {MarkdownBookPreviewServer} = require('./lib/markdown-book-preview-server');
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,14 +17,11 @@ function activate(context) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "mdbp-vscode" is now active!');
 
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.test', ()=>{
-    vscode.window.showInformationMessage('Hello! This is Test.');
-  }));
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.startServer', startServer));
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.stopServer', stopServer));
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.toggle', toggle));
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.toggleVS', toggleVS));
-  context.subscriptions.push(vscode.registerCommand('mdbp-vscode.exportXML', exportXML));
+  context.subscriptions.push(vscode.commands.registerCommand('mdbp-vscode.startServer', startServer));
+  context.subscriptions.push(vscode.commands.registerCommand('mdbp-vscode.stopServer', stopServer));
+  context.subscriptions.push(vscode.commands.registerCommand('mdbp-vscode.toggle', toggle));
+  context.subscriptions.push(vscode.commands.registerCommand('mdbp-vscode.toggleVS', toggleVS));
+  context.subscriptions.push(vscode.commands.registerCommand('mdbp-vscode.exportXML', exportXML));
 
 
   // サーバーの起動終了
@@ -37,10 +35,10 @@ function activate(context) {
   // 生のHTMLで開く
   function toggle(vsmode = false) {
     console.log('MarkdownBookPreview was toggled!');
-    const editor = vscode.workspace.getActiveTextEditor();
+    const editor = vscode.window.activeTextEditor;
     if (checkEditorPath(editor) === false) return;
     // プレビューしたいパスやVSmodeを設定
-    const mdpath = editor.getPath();
+    const mdpath = editor.document.fileName;
     console.log(mdpath);
     markdownBookPreviewServer.setTargetMarkdownFile(mdpath, vsmode);
   }
@@ -54,23 +52,23 @@ function activate(context) {
   // InDesign用のXMLを書き出す
   function exportXML() {
     console.log('MarkdownBookPreview export XML');
-    const editor = vscode.workspace.getActiveTextEditor();
+    const editor = vscode.window.activeTextEditor;
     if (checkEditorPath(editor) === false) return;
     // プレビューしたいパスやVSmodeを設定
-    const mdpath = editor.getPath();
+    const mdpath = editor.document.fileName;
     markdownBookPreviewServer.exportInDesignXML(mdpath);
   }
 
   // チェック
   function checkEditorPath(editor) {
-    if (editor === null) return false;
-    const path = editor.getPath();
+    if (editor === null || editor === undefined) return false;
+    const path = editor.document.fileName;
     if (!path) {
-        vscode.window.showInformationMessage('ファイルを保存してから実行してください');
+        vscode.window.showWarningMessage('ファイルを保存してから実行してください');
         return false;
     }
-    if (path.indexOf('.md') < 0) {
-        vscode.window.showInformationMessage('Mardownファイルではありません');
+    if (!(path.endsWith('.md'))) {
+        vscode.window.showWarningMessage('Mardownファイルではありません');
         return false;
     }
     return true;
